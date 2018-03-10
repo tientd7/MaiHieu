7bb0eff3192320bf939666fc64ddf52c11fba862
Mai Hieu <Mai Hieu@MAIHIEU-PC>
2018-03-10 22:57:41 +0700

    ok rồi

---
+++ NerahomeWeb-master (1)/NerahomeWeb-master/NeraHomeWebApp/NerahomeWeb/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NerahomeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Tesst()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Tesest()
+        {
+
+            ViewBag.Message = "ok di";
+            return View();
+        }
+    }
+}
